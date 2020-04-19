ca64470e83377d4ee3567f2a85549c5af2ac89a8
oscar <osgarc05@ucm.es>
2020-04-19 10:55:35 +0200

    Arreglado el rayo del flasher

--- Assets/Scripts/Enemies/Flasher/FlasherRay.cs
+++ Assets/Scripts/Enemies/Flasher/FlasherRay.cs
@@ -13,13 +13,11 @@ public class FlasherRay : MonoBehaviour
 
     Rigidbody2D rb;
     Vector3 flasher;
-    
 
-    void Awake()
+
+    void Start()
     {
         player = GameManager.instance.GetPlayer().transform;    //Almacena el transform del player
-
-        timeDuration = duration + Time.time;        //Inicializacion contador tiempo
 
         if (player != null)
         {
@@ -28,16 +26,17 @@ public class FlasherRay : MonoBehaviour
             rb = GetComponent<Rigidbody2D>();
             rb.velocity = (player.position - flasher).normalized * speed;       //Nomraliza el vector que une flasher-player y lo multiplpica por speed=Velociad del rayo
             transform.parent = null;
-            Vector3 diference = player.position - transform.position;           //Calcula la distancia entre player y flasher
+            Vector3 diference = player.position - flasher;           //Calcula la distancia entre player y flasher
             float rotationz = Mathf.Atan2(diference.y, diference.x) * Mathf.Rad2Deg;    //Rota el rayo para que apunte al jugador
             transform.rotation = Quaternion.Euler(0f, 0f, rotationz);                   
         }
     }
     void FixedUpdate()
     {        
-        Vector3 pos = rb.transform.position;        
-        Vector3 increm = new Vector3((flasher.x- pos.x)*2, 0.5f, 1);        //Increm.x calcula la distancia entre flasher y player y la duplica(aumento a izq y dcha)
-        transform.localScale = increm;       
+        Vector3 pos = rb.transform.position;
+        float modulo = Convert.ToSingle(Math.Sqrt(((flasher.x - pos.x) * (flasher.x - pos.x)) + ((flasher.y - pos.y) * (flasher.y - pos.y))));
+        Vector3 increm = new Vector3(modulo*2, 0.5f, 1);        //Increm.x calcula la distancia entre flasher y player y la duplica(aumento a izq y dcha)
+        transform.localScale = increm;        
     }
     void OnCollisionEnter2D(Collision2D collision)     
     {
