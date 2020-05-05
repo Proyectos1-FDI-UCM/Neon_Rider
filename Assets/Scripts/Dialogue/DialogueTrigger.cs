49daa27b1786b9c6d625d1a4a6b468e6fda0b0f8
Sergio Alfonso <sergialf@ucm.es>
2020-05-05 20:59:55 +0200

    Creado el sistema de diálogos

---
+++ Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+// Comienza el diálogo cuando el jugador entra en el trigger del checkpoint 
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+    public DialogueManager diaMan;
+    int checkpointDeadVal;
+
+    void Start()
+    {
+        checkpointDeadVal = GetComponent<Checkpoint>().deadVal;
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Comienza el diálogo si no ha pasado todavía por el checkpoint que lo contiene
+        if (collision.GetComponent<PlayerController>() != null && GameManager.instance.deadVal != checkpointDeadVal)
+        {
+            diaMan.StartDialogue(dialogue);
+            this.enabled = false;
+        }
+    }
+}
