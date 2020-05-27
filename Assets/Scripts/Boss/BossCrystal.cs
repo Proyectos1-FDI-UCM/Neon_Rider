b1a4d542a4180c6e227011cc5160d0f0662a0f84
Sergio <Alfonso>
2020-05-28 00:33:33 +0200

    Añadida la animación de destrucción de cristales

--- Assets/Scripts/Boss/BossCrystal.cs
+++ Assets/Scripts/Boss/BossCrystal.cs
@@ -6,7 +6,15 @@ using UnityEngine;
 public class BossCrystal : MonoBehaviour
 {
     [SerializeField] BossBehaviour boss = null;
+    Animator anim;
+    Transform child;
     bool active = false;
+
+    void Start()
+    {
+        child = transform.GetChild(0);
+        anim = child.GetComponent<Animator>();
+    }
     public void Break()
     {
         if (active && boss != null)
@@ -15,6 +23,14 @@ public class BossCrystal : MonoBehaviour
 
             boss.UpdateCrystal();
 
+            // Separamos al hijo del padre
+            // Llamamos a Animation de "EnemyDeathAnim"
+            if (anim != null)
+            {
+
+                anim.SetBool("Broken", true);
+                child.SetParent(null);
+            }
             Destroy(this.gameObject);
         }
     }
