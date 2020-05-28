fed08200f2a3d6f2d13a1b15623d9d26d8085565
oscar <osgarc05@ucm.es>
2020-05-28 11:36:00 +0200

    Arreglada la muerte de la torreta y las trampas no rompen balas

--- Assets/Scripts/Enemies/Enemy_Death.cs
+++ Assets/Scripts/Enemies/Enemy_Death.cs
@@ -29,6 +29,20 @@ public class Enemy_Death : MonoBehaviour
         // Si no es el ralentizador, drone o torreta, cogemos al hijo
         if ((enemy != null||ralen!=null) && drone == null && turret == null)
             child = transform.GetChild(0);
+        else if (turret != null && ralen==null && drone==null)
+        {
+            int i = 0;
+            bool sprite = false;
+            while (!sprite && transform.GetChild(i) != null)
+            {
+                if(transform.GetChild(i).GetComponent<Animator>()!=null)
+                {
+                    child = transform.GetChild(i);
+                    sprite = true;
+                }
+                i++;    
+            }
+        }
     }
     // Metodo llamado desde el componente Sword_Attack
     public void OnAttack()
@@ -38,14 +52,14 @@ public class Enemy_Death : MonoBehaviour
    
         if (hitsToDeath == 0)
         {
-            if ((enemy != null || ralen != null || prest != null) && turret == null && drone == null)
+            if ((enemy != null || ralen != null || prest != null || turret != null) && drone == null)
             {
                 // Separamos al hijo del padre
                 // Llamamos a Animation de "EnemyDeathAnim"
                 if (child.GetComponent<Animator>() != null)
                 {
                     child.GetComponent<Animator>().SetBool("Death", true);
-                    if (prest == null && ralen == null)
+                    if (prest == null && ralen == null && turret==null)
                         child.GetChild(0).GetComponentInChildren<SpriteRenderer>().enabled = false;
                     child.SetParent(null);
                 }
